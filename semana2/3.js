b5c19ca1be6f89ba1f6fead4e333929886dd366a
Marcio Holanda <marcioh.dev@gmail.com>
2023-07-20 19:05:22 -0300

    Resolução do exercício 3

---
+++ semana2/3.js
@@ -0,0 +1,6 @@
+var nome = prompt("Informe o seu nome: ")
+var nota1 = prompt("Informe sua primeira nota: ")
+var nota2 = prompt("Infome sua segunda nota: ")
+media = (nota1 + nota2) / 2;
+
+document.write("A média do aluno " + nome + " é: " + media);
