96f88c780e7fab78f246fb41ab5bd65193695c65
Marcio Holanda <marcioh.dev@gmail.com>
2023-07-20 18:45:31 -0300

    Create 1.js
    Resolução do ecercício 1 e 2

---
+++ semana2/1.js
@@ -0,0 +1,12 @@
+1 e 2
+
+let valor1 = parseInt(prompt("Insira o primeiro valor: "));
+
+let valor2 = parseInt(prompt("Insira o segundo valor: "));
+
+let valor3 = parseInt(prompt("Insira o terceiro valor: "));
+
+let media = (valor1 + valor2 + valor3) / 3;
+
+alert("As 3 notas são: " + valor1 + " - " + valor2 + " - " + valor3);
+alert("A média é: " + media);
