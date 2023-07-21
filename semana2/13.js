ea2986198906b072bf7f8d854abe090248a88301
Marcio Holanda <marcioh.dev@gmail.com>
2023-07-20 21:14:57 -0300

    Update 13.js

--- semana2/13.js
+++ semana2/13.js
@@ -2,7 +2,7 @@
 
 var numero = parseInt(prompt("Insira um número: "));
 
-//O %(mod) retorna o resto da divisao. A verificação aqui vê se o resto da divisão 
+//O %(mod) retorna o resto da divisao. A verificação aqui vê se o resto da divisão é igual a 0. Se for, isso significa que o número é PAR.
 if (numero % 2 == 0) {
 	document.write("É PAR!");
 } else {
