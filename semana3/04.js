9a885c6309037d88b661caa60527445f2a166459
Marcio Holanda <marcioh.dev@gmail.com>
2023-08-01 21:00:51 -0300

    Resolvido não testado

--- semana3/04.js
+++ semana3/04.js
@@ -1,1 +1,10 @@
-//Calcular a área de um triângulo
+//Calcular a área de um triângulo de acordo com os valores de base e altura informados. 
+
+function calcularArea(base, altura){
+  return (base * altura) / 2;
+}
+
+let base = parseFloat(prompt("Informe o comprimento da base do triângulo: "));
+let altura = parseFloat(prompt("Informe a altura do triângulo: "));
+
+document.write("A area do triangulo é: " + calcularArea(base, altura));
