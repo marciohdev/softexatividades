28cdc7617003b338ec439cd9445d9179adcfebc5
Marcio Holanda <marcioh.dev@gmail.com>
2023-08-10 18:39:23 -0300

    criado transporte.js

---
+++ portal/transporte.js
@@ -0,0 +1,18 @@
+let eterrestre = false;
+let cabeSoUm = false;
+let usaCapacete = false; 
+let ePesado = false; 
+let temPedal = false; 
+
+
+if(cabeSoUm){
+  
+  if(ePesado){
+    console.log("É trator!")
+  } else if(temPedal){
+    console.log("É bicicleta")
+  }
+
+} else if(usaCapacete){
+  console.log("É moto!")
+}
