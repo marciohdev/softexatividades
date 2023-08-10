4f6b2f59214ff9918acaa59c54cf5676ecf8341a
Marcio Holanda <marcioh.dev@gmail.com>
2023-08-10 12:45:13 -0300

    atividade do portal - variáveis

---
+++ portal/variáveis.js
@@ -0,0 +1,11 @@
+let nome;
+let cpf; 
+let rg;
+let altura; 
+let endereco; 
+
+nome = "José Almeida da Silva"
+cpf = 12345678900;
+rg = 9517530;
+altura = 1,78;
+endereco = "Rua A, 380 - Centro - Recife/PE"
