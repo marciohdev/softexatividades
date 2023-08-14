9cdeb0af525103746fba94cc7093ed3caaa000a8
Marcio Holanda <marcioh.dev@gmail.com>
2023-08-14 13:48:36 -0300

    Create listas.js

---
+++ portal/listas.js
@@ -0,0 +1,15 @@
+let pessoas = ["Marcio", "Lucas", "Maria", "Valentina"];
+let idades = [10, 28, 69, 40];
+let cores = ["Azul", "Branco", "Rosa"];
+
+console.log(pessoas)
+console.log(idades)
+console.log(cores)
+
+pessoas.unshift("Adicionado");
+idades[0] = 50;
+cores.pop();
+
+console.log(pessoas)
+console.log(idades)
+console.log(cores)
