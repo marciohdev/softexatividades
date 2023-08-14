f26a7ede51cb0125fa61d38addd4946b06457211
Marcio Holanda <marcioh.dev@gmail.com>
2023-08-14 12:01:36 -0300

    Create valores.js

---
+++ portal/valores.js
@@ -0,0 +1,11 @@
+let nome;
+let idade; 
+let altura; 
+let maior_de_idade;
+
+nome = "Marcio Leandro" 
+idade = 19; 
+altura = 1.90; 
+maior_de_idade = true;
+
+console.log(nome, idade, altura, maior_de_idade);
