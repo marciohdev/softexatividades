5a0ca4c47c653b13fbedc9e97d75ede1340e33dc
Marcio Holanda <marcioh.dev@gmail.com>
2023-09-01 12:33:00 -0300

    Create matrizAnimais.js

---
+++ portal/matrizAnimais.js
@@ -0,0 +1,15 @@
+matrizAnimais = [
+
+    ["Peludo", "Mamífero", "Pequeno"],
+    ["Penas", "Tem bico", "Pequeno"],
+    ["Peludo", "Tem patas", "Tem fucinho"]
+
+]
+
+for (let l = 0; l < 3; l++) {
+    for (let c = 0; c < 3; c++) {
+
+        console.log(matrizAnimais[l][c])
+    }
+}
+
